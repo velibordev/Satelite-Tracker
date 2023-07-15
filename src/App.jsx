8a53982dfe0400b7d34657d12632944a6a68db36
Velibor Minić <138766482+velibordev@users.noreply.github.com>
2023-07-15 21:07:56 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="header_text">Track ISS Satelite Data</h1>
+        <div className="square_data">
+          <div>
+            <p>Latitude</p>
+            <span>123.2213</span>
+          </div>
+          <div>
+            <p>Latitude</p>
+            <span>123.2213</span>
+          </div>
+          <div>
+            <p>Latitude</p>
+            <span>123.2213</span>
+          </div>
+          <div>
+            <p>Latitude</p>
+            <span>123.2213</span>
+          </div>
+          <div>
+            <p>Latitude</p>
+            <span>123.2213</span>
+          </div>
+          <div>
+            <p>Latitude</p>
+            <span>123.2213</span>
+          </div>
+        </div>
+        <div id="map"></div>
+      </div>
+    </>
+  );
+}
